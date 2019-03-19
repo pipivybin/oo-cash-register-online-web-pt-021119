7e657d1800db7b63732137d9bcc78ce8419f11e2
Ming Duan <11203duan@gmail.com>
2019-03-19 21:24:08 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,29 @@
 
+
+class CashRegister
+
+@@total = 0
+@@all_items = []
+
+def new(discount = 0)
+  @total = 0
+  @discount = discount
+end
+
+def total
+  @@total
+end
+
+def add_item(title, price, quantity = 1)
+  @total += 1
+end
+
+def apply_discount
+
+end
+
+def items
+  @@all_items
+end
+
+end
