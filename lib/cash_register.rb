9ca6a7949b48c0a2bb1efb6610c0ffbeed7bbc03
Ming Duan <11203duan@gmail.com>
2019-03-19 21:43:52 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,6 +4,8 @@ class CashRegister
 
 attr_accessor :discount :items
 
+@item = []
+
 def initialize(discount = 0)
   @total = 0
   @discount = discount
@@ -19,7 +21,6 @@ end
 
 def add_item(title, price, quantity = 1)
   @total += price * quantity
-  @item = []
   @item << self
 end
 
